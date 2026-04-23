f7d9aeb7ecf142f62d020b5d657ca00f8cfdf4e1
perez <perezalexandre430@gmail.com>
2026-04-23 20:00:51 +0200

    add admin

---
+++ sanity.cli.ts
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'csl3lf8y',
+    dataset: 'production',
+  },
+  studioHost: 'echiquier-martinerois',
+  deployment: {
+    appId: 'p6my38w9xfzec4kxhxnvdk4d',
+  },
+})
